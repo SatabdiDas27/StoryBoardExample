cb57926b7aa4ed432fb66989a449236df617ad93
Satabdi Das <satabdidas@BHUKRK458L.local>
2018-04-30 14:10:34 -0700

    storyboard structures

--- TableViewStoryBoard/TableViewStoryBoard/ViewController.swift
+++ TableViewStoryBoard/TableViewStoryBoard/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Satabdi here")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
